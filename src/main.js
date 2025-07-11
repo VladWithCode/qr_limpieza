5976e4b403525def7a09c9ec0229e80685269302
vladwb <vladwithcode@gmail.com>
2025-07-10 21:13:57 -0600

    feat: add animations

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const COLOR_PRIMARY = '#009473FF';
 const COLOR_PRIMARY_TRANSPARENT = '#00947300';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let isMenuOpen = false;
     const menuToggler = document.querySelector('#menu-toggler');
     const topOsbv = new IntersectionObserver(handleTopIntersection);
     const viewAnimateObsv = new IntersectionObserver(handleViewAnimateIntersection);
@@ -16,87 +15,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     topOsbv.observe(document.getElementById('top-osbv-target'));
-    menuToggler.addEventListener('click', () => {
-        if (!isMenuOpen) {
-            document.getElementById('menu').setAttribute('data-mobile-state', 'open');
-            gsap.to('#menu', {
-                duration: 0.2,
-                y: '3rem',
-            });
-            gsap.to('[data-toggler-line="2"]', {
-                duration: 0.2,
-                width: '0%',
-            });
-            gsap.set('[data-toggler-line]:not([data-toggler-line="2"])', {
-                position: 'absolute',
-                y: '-50%',
-                x: '-50%',
-                top: '50%',
-                left: '50%',
-            });
-            gsap.to('[data-toggler-line="1"]', {
-                rotate: '-45deg',
-                duration: 0.2,
-            });
-            gsap.to('[data-toggler-line="3"]', {
-                rotate: '45deg',
-                duration: 0.2,
-            });
-
-            if (document.getElementById('header').getAttribute('data-at-top') === '1') {
-                gsap.to('#header', {
-                    duration: 0.2,
-                    background: COLOR_PRIMARY,
-                    position: 'fixed',
-                }, "<");
-            }
-        } else {
-            document.getElementById('menu').setAttribute('data-mobile-state', 'closed');
-            gsap.to('#menu', {
-                duration: 0.2,
-                y: '-100%',
-            });
-            gsap.to('[data-toggler-line="2"]', {
-                duration: 0.2,
-                width: '1.25rem',
-            });
-            gsap.set('[data-toggler-line]:not([data-toggler-line="2"])', {
-                position: 'relative',
-                y: '',
-                x: '',
-                top: '',
-                left: '',
-            });
-            gsap.to('[data-toggler-line="1"]', {
-                rotate: '0deg',
-                duration: 0.2,
-            });
-            gsap.to('[data-toggler-line="3"]', {
-                rotate: '0deg',
-                duration: 0.2,
-            });
-
-            if (document.getElementById('header').getAttribute('data-at-top') === '1') {
-                gsap.to('#header', {
-                    duration: 0.2,
-                    background: COLOR_PRIMARY_TRANSPARENT,
-                    stagger: 0.05,
-                    position: 'absolute',
-                });
-            }
-        }
-
-        isMenuOpen = !isMenuOpen;
-    });
+    menuToggler.addEventListener('click', handleMenuToggle);
 
     function handleTopIntersection(ents) {
-        const ent = ents[0];
-        const header = document.getElementById('header');
         const mobOpen = document.getElementById('menu').getAttribute('data-mobile-state') === 'open';
-
         if (mobOpen) {
             return
         }
+
+        const ent = ents[0];
+        const header = document.getElementById('header');
 
         if (ent.isIntersecting) {
             header.setAttribute('data-at-top', 1);
@@ -109,12 +37,67 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function handleMenuToggle() {
+        const tl = gsap.timeline({ defaults: { duration: 0.35, ease: 'power2.inOut' } });
+        const isOpen = document.getElementById('menu').getAttribute('data-mobile-state') === 'open';
+
+        if (!isOpen) {
+            document.getElementById('menu').setAttribute('data-mobile-state', 'open');
+            tl.to('#menu', {
+                y: '3rem',
+            }).to('[data-toggler-line="2"]', {
+                width: '0%',
+            }, 0).set('[data-toggler-line]:not([data-toggler-line="2"])', {
+                position: 'absolute',
+                y: '-50%',
+                x: '-50%',
+                top: '50%',
+                left: '50%',
+            }).to('[data-toggler-line="1"]', {
+                rotate: '-45deg',
+            }, "<").to('[data-toggler-line="3"]', {
+                rotate: '45deg',
+            }, "<");
+
+            if (document.getElementById('header').getAttribute('data-at-top') === '1') {
+                gsap.to('#header', {
+                    background: COLOR_PRIMARY,
+                    position: 'fixed',
+                }, 0);
+            }
+        } else {
+            document.getElementById('menu').setAttribute('data-mobile-state', 'closed');
+            tl.to('#menu', {
+                y: '-100%',
+            }).to('[data-toggler-line="2"]', {
+                width: '1.25rem',
+            }, "<").set('[data-toggler-line]:not([data-toggler-line="2"])', {
+                position: 'relative',
+                y: '',
+                x: '',
+                top: '',
+                left: '',
+            }).to('[data-toggler-line="1"]', {
+                rotate: '0deg',
+            }, 0).to('[data-toggler-line="3"]', {
+                rotate: '0deg',
+            }, 0);
+
+            if (document.getElementById('header').getAttribute('data-at-top') === '1') {
+                tl.to('#header', {
+                    background: COLOR_PRIMARY_TRANSPARENT,
+                    position: 'absolute',
+                }, 0);
+            }
+        }
+    }
+
     function handleViewAnimateIntersection(ents, obsv) {
         for (let ent of ents) {
             if (ent.isIntersecting) {
                 const el = ent.target;
                 const animate = el.getAttribute('data-view-animate');
-                
+
                 animations[animate](el, {
                     stagger: Number(el.dataset.viewAnimateStagger) || 0,
                     delay: Number(el.dataset.viewAnimateDelay) || 0,
