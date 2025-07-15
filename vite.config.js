e768c25bce307bee74121cf6be907abb6e2e371f
vladwb <vladwithcode@gmail.com>
2025-07-14 19:17:59 -0600

    fix: asset path error

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    //base: '/qr_limpieza/',
+    base: '/qr_limpieza/',
     plugins: [
         tailwindcss(),
     ]
